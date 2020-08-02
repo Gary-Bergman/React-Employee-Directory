b53157ba889dc7add38f2f1ec9e64d290c7a6c50
Gary Bergman <garybergman00@gmail.com>
2020-08-02 16:36:31 -0400

    Created title component to act as page header.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Title from "./components/Title";
-import Search from "./components/Search";
-import Table from "./components/Table";
+// import Search from "./components/Search";
+// import Table from "./components/Table";
 
 
 function App() {
   return (
     <>
       <Title>Test</Title>
-      <Search></Search>
-      <Table></Table>
+      {/* <Search></Search>
+      <Table></Table> */}
     </>
   );
 }
---
+++ src/components/Title/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+function Title() {
+    return (
+        <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+          <h1 className="display-4">Employee Directory</h1>
+          <p className="lead">Click on carrots to filter by heading or use the search box to narrow your results.</p>
+        </div>
+      </div>
+      
+  );
+}
+
+export default Title;
